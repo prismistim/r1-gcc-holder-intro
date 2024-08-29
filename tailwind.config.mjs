ea78b3a4229c7855811a626bd1dbc729695225c9
snowsphere <info@snowsphere.net>
2024-08-29 16:24:28 +0900

    feat: fontの追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			...colors,
+      primary: '#f43f5e',
+      'primary-content': '#262626',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['"Rubik Variable"', '"M PLUS 2 Variable"', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
